305a6d8500c52c077375326b06484144822388cd
litsynp <nocte55is@gmail.com>
2021-12-03 03:08:56 +0900

    feat: update security to enable H2 console

--- src/main/kotlin/com/litsynp/application/global/config/SecurityConfig.kt
+++ src/main/kotlin/com/litsynp/application/global/config/SecurityConfig.kt
@@ -1,10 +1,19 @@
 package com.litsynp.application.global.config
 
+import org.slf4j.LoggerFactory
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.access.expression.SecurityExpressionHandler
+import org.springframework.security.access.hierarchicalroles.RoleHierarchy
+import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl
+import org.springframework.security.access.hierarchicalroles.RoleHierarchyUtils
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.web.FilterInvocation
+import org.springframework.security.web.access.expression.DefaultWebSecurityExpressionHandler
+import java.util.*
 
 
 @Configuration
@@ -16,8 +25,38 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 )
 class SecurityConfig : WebSecurityConfigurerAdapter() {
 
+    private val log = LoggerFactory.getLogger(javaClass)
+
+    @Bean
+    fun roleHierarchy(): RoleHierarchy? {
+        val roleHierarchy = RoleHierarchyImpl()
+        val roleHierarchyMap: MutableMap<String, List<String>> = HashMap()
+        roleHierarchyMap["ROLE_ADMIN"] = Arrays.asList("ROLE_USER")
+        val roles = RoleHierarchyUtils.roleHierarchyFromMap(roleHierarchyMap)
+        log.info(roles)
+        roleHierarchy.setHierarchy(roles)
+        return roleHierarchy
+    }
+
+    @Bean
+    fun expressionHandler(): SecurityExpressionHandler<FilterInvocation?>? {
+        val webSecurityExpressionHandler = DefaultWebSecurityExpressionHandler()
+        webSecurityExpressionHandler.setRoleHierarchy(roleHierarchy())
+        return webSecurityExpressionHandler
+    }
+
     @Throws(Exception::class)
-    override fun configure(security: HttpSecurity) {
-        security.httpBasic().disable()
+    override fun configure(http: HttpSecurity) {
+        http
+            .httpBasic()
+            .and()
+            .csrf().disable()
+            .exceptionHandling()
+            .and()
+            .headers().frameOptions().disable() // enable H2-console
+            .and()
+            .authorizeRequests().expressionHandler(expressionHandler())
+            .anyRequest().permitAll()
+
     }
 }
