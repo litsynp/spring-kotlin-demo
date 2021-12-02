498e934ae17dd21b83113ca6a94cb74fe1ad8be5
litsynp <nocte55is@gmail.com>
2021-12-03 01:40:02 +0900

    feat: create BaseTimeEntity

--- src/main/kotlin/com/litsynp/application/Application.kt
+++ src/main/kotlin/com/litsynp/application/Application.kt
@@ -2,10 +2,12 @@ package com.litsynp.application
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
 @SpringBootApplication
+@EnableJpaAuditing
 class Application
 
 fun main(args: Array<String>) {
-	runApplication<Application>(*args)
+    runApplication<Application>(*args)
 }
