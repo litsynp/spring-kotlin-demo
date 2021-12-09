6b7344d27a5145e306039af3740bd10682bba14d
litsynp <nocte55is@gmail.com>
2021-12-10 01:54:06 +0900

    refactor: remove unused imports and variables

--- src/main/kotlin/com/litsynp/application/domain/user/controller/UserController.kt
+++ src/main/kotlin/com/litsynp/application/domain/user/controller/UserController.kt
@@ -3,17 +3,8 @@ package com.litsynp.application.domain.user.controller
 import com.litsynp.application.domain.user.dto.UserMapper
 import com.litsynp.application.domain.user.dto.request.SignUpRequestDto
 import com.litsynp.application.domain.user.dto.response.UserResponseDto
-import com.litsynp.application.domain.user.entity.ERole
-import com.litsynp.application.domain.user.entity.Role
-import com.litsynp.application.domain.user.entity.User
-import com.litsynp.application.domain.user.exception.RoleNotFoundException
-import com.litsynp.application.domain.user.exception.UserDuplicateException
-import com.litsynp.application.domain.user.repository.RoleRepository
-import com.litsynp.application.domain.user.repository.UserRepository
 import com.litsynp.application.domain.user.service.UserService
-import com.litsynp.application.global.security.jwt.JwtUtils
 import org.springframework.http.ResponseEntity
-import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.web.bind.annotation.*
 import java.util.*
 import javax.validation.Valid
@@ -23,9 +14,7 @@ import javax.validation.Valid
 @RequestMapping("/api/v1/users")
 class UserController(
     val userService: UserService,
-    val userRepository: UserRepository,
     val userMapper: UserMapper,
-    val jwtUtils: JwtUtils
 ) {
     @GetMapping("/{id}")
     fun getById(@PathVariable id: UUID): UserResponseDto {
